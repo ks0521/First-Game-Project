4acf00807fb61556529d39be5b6632d7789e2b1b
GyuSeong <moon01087209503@gmail.com>
2025-12-09 15:19:40 +0900

    충돌 테스트용 파일제작
    충돌 확인용

---
+++ Assets/_GameName/Scripts/Utills/CollisionTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _GameName.Scripts.Utills
+{
+    public class CollisionTest : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("충돌테스트 1");
+        }
+
+    }
+}
