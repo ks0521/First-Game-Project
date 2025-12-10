b334fe965a062153dfa98d9393b76a10487c7df7
GyuSeong <moon01087209503@gmail.com>
2025-12-10 18:38:55 +0900

    공용 인터페이스&enum 저장소
    새로 만드실 인터페이스나 enum은 여기다가 저장해주세요!!!!!

---
+++ Assets/_Kamikakushi/Scripts/Utills/Interface_Enum.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Kamikakushi.Utills
+{
+    public class Interface_Enum
+    {
+        enum layerMask
+        {
+
+        }
+    }
+}
+
