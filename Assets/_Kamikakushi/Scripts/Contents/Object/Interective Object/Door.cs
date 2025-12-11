39a3a03ac8df5339616250bd433813b6529ae27f
GyuSeong <moon01087209503@gmail.com>
2025-12-11 18:03:17 +0900

    피격시 화면 흔들림 / 고정 구현

--- Assets/_Kamikakushi/Scripts/Contents/Object/Interective Object/Door.cs
+++ Assets/_Kamikakushi/Scripts/Contents/Object/Interective Object/Door.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using _Kamikakushi.Utills;
+using _Kamikakushi.Contents.Player;
 using UnityEngine;
 
 public class Door : MonoBehaviour, IInteractable
--- Assets/_Kamikakushi/Scripts/Utills/RequireItemKeyCode.cs
+++ Assets/_Kamikakushi/Scripts/Utills/RequireItemKeyCode.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using _Kamikakushi.Contents.Player;
 using _Kamikakushi.Utills;
 using UnityEngine;
 
