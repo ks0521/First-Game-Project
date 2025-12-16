253341a3b6eedb8227d828c5edbbbd0ca3c9ad4f
dlrugn11-lab <dlrugn11@gmail.com>
2025-12-16 13:23:22 +0900

    오브젝트
    아이템과 오브젝트 플레이어 상호작용
    스크립트추가및 오브젝트 추가

---
+++ Assets/_Kamikakushi/Scripts/Utills/Enums/PromptKey.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PromptKey
+{
+    None = 0,
+
+    //문관련
+    OpenDoor, //특정아이템필요없는 문열기
+    LockDoor, //특정아이템이필요한 문열기
+    CloseDoor, //문닫기
+    
+    //아이템관련
+    PickupItem, //아이템 줍기
+    UseItem, //아이템 사용하기
+
+    //환경 상호작용
+    AcivateSwitch, //스위치(필요할수도있을것같아서)
+    Insprct, //조사
+
+    //숨기
+    Hide, //숨기
+    HideTransform //숨기후 카메라 시점이동 (장롱틈으로 시선이동하는 느낌)
+}
