a88b7b52130067de170549c9455bb55a6bb7f3cc
GyuSeong <moon01087209503@gmail.com>
2025-12-16 13:52:01 +0900

    디텍터블 인터페이스 생성

---
+++ Assets/_Kamikakushi/Scripts/Utills/Interfaces/IDetectable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Kamikakushi.Utills.Interfaces
+{
+    public class IDetectorble : MonoBehaviour
+    {
+        public bool CanDetected {  get; private set; }
+    }
+}
+
