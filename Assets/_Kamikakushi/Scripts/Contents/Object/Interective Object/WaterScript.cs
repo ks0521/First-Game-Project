6202c26f22e2bb848bcf926965f2f3deab78830b
GyuSeong <moon01087209503@gmail.com>
2025-12-17 23:38:37 +0900

    오류 수정
    오류 수정본

--- Assets/_Kamikakushi/Scripts/Contents/Object/Interective Object/WaterScript.cs
+++ Assets/_Kamikakushi/Scripts/Contents/Object/Interective Object/WaterScript.cs
@@ -2,24 +2,32 @@
 using _Kamikakushi.Contents.Player;
 using _Kamikakushi.Utills.Interfaces;
 using _Kamikakushi.Utills.Enums;
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static System.Runtime.CompilerServices.RuntimeHelpers;
+using _Kamikakushi.Utills.Structs;
 
-public class WaterScript : InteractItems, IInteractable
+public class WaterScript : InteractItems
 {
-    public bool Interact(_Kamikakushi.Contents.Player.PlayerManager target)
+    public override InteractResult Interact(PlayerManager target)
     {
         //마시면 정신력을 회복시켜주는 오브젝트
-        target.sanity += 20;
-        return true;
+        InteractResult result;
+        result.success = true;
+        result.message = "정신이 맑아진 기분이 든다...";
+        result.transform = null;
+        target.stat.sanity += 20;
+        return result;
     }
     protected override void Init()
     {
         //인터페이스의 배열이기때문에 GetComponents 사용
         conditions = GetComponents<IInteractionCondition>();
         interactType = InteractType.Event;
+        context.promptKey = PromptKey.UseItem;
+        context.displayName = "물";
     }
 
 }
