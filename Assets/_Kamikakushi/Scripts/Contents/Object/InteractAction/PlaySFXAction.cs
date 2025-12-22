50541ddc933a19bf2574b9e8ba6004a6d26e6cb0
GyuSeong <moon01087209503@gmail.com>
2025-12-22 23:17:12 +0900

    픽업아이템 줍기 - 오디오 연동
    이제 픽업아이템을 주울때마다 소리가 발생

---
+++ Assets/_Kamikakushi/Scripts/Contents/Object/InteractAction/PlaySFXAction.cs
@@ -0,0 +1,31 @@
+﻿using _Kamikakushi.Contents.Player;
+using _Kamikakushi.Utills.Audio;
+using _Kamikakushi.Utills.Interfaces;
+using Project.Inventory;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+namespace _Kamikakushi.Contents.InteractAction
+{
+    public class PlaySFXAction : IInteractAction
+    {
+        PlayerAudio playerAudio;
+        private readonly SFXType type;
+
+        public PlaySFXAction(SFXType _type)
+        {
+            type = _type;
+        }
+        public void Execute(PlayerManager player, IInteractable obj)
+        {
+            playerAudio = player.gameObject.GetComponent<PlayerAudio>();
+            if(playerAudio == null)
+            {
+                Debug.LogWarning("플레이어 오디오 불러오기 오류");
+            }
+            playerAudio.PlaySFX(type);
+            Debug.Log("플레이어sfx");
+        }
+    }
+}
---
+++ Assets/_Kamikakushi/Scripts/Contents/Player/PlayerAudio.cs
@@ -0,0 +1,43 @@
+﻿using _Kamikakushi.Audio;
+using _Kamikakushi.Utills.Audio;
+using _Kamikakushi.Utills.Interfaces;
+using _Kamikakushi.Utills.Structs;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace _Kamikakushi.Contents.Player
+{
+    public class PlayerAudio : MonoBehaviour
+    {
+        [SerializeField] private AudioSource playerSfx; //피격, 발소리 등 플레이어 관련 sfx
+        [SerializeField] private AudioSource heartbeat; //추격시 심장소리
+        [SerializeField] private AudioSource breath; //숨었을 때 숨소리
+
+        private AudioClip sfxClip;
+        public void PlaySFX(SFXType type)
+        {
+            if (playerSfx == null) return;
+            sfxClip = AudioManager.Instance.GetSFX(type);
+            if(sfxClip == null)
+            {
+                Debug.LogWarning($"{type} 타입 SFX 가져오기 실패");
+                return;
+            }
+            playerSfx.PlayOneShot(sfxClip);
+            Debug.Log("플레이어 음성실행 성공");
+        }
+
+        public void PlayHeartbeat()
+        {
+            if(heartbeat == null) return;
+            if(AudioManager.Instance.GetLoop(SFXType.HideBreath) == null)
+            {
+                Debug.LogWarning("숨기 사운드 가져오기 실패");
+                return;
+            }
+            heartbeat.PlayOneShot(AudioManager.Instance.GetLoop(SFXType.HideBreath));
+        }
+    }
+}
