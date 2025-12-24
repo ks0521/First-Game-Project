c3d2a8fc8a6209bc10b76bd1c520d821aa0e1b6b
GyuSeong <moon01087209503@gmail.com>
2025-12-24 16:51:07 +0900

    아이템 생성
    읽기 아이템
    1. 책
    2. 편지
    3. 문양
    4. 그림
    상호작용 아이템
    1. 피묻은 천
    사용 아이템
    1. 물(정신력 회복)

---
+++ Assets/_Kamikakushi/Scripts/Contents/Object/Interective Object/BloodedCloth.cs
@@ -0,0 +1,38 @@
+﻿using _Kamikakushi.Contents.Item;
+using _Kamikakushi.Contents.Player;
+using _Kamikakushi.Utills.Enums;
+using _Kamikakushi.Utills.Interfaces;
+using _Kamikakushi.Utills.Structs;
+using DoorScript;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Kamikakushi.Contents.InteractiveObject
+{
+    public class BloodedCloth : InteractItems
+    {
+        //이벤트 진행이 되었는지 판정(이미 진행됬으면 또 읽어도 이벤트 진행이 되지않음 - 중복방지)
+        bool isTriggered;
+        protected override void Init()
+        {
+            //인터페이스의 배열이기때문에 GetComponents 사용
+            interactType = InteractType.Event;
+            
+            context.promptKey = PromptKey.Inspect;
+            context.displayName = "피묻은 천";
+        }
+        public override InteractResult Interact(PlayerManager target)
+        {
+            result.success = true;
+            result.message = "피가 말라붙어 있는 더러운 천이다";
+
+            if (!isTriggered)
+            {
+                //특정 액션
+            }
+            //아니면 result.actions.add(여기에 특정 트리거 추가)
+            return result;
+        }
+    }
+}
