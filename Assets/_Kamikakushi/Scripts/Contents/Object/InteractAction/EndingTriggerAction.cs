b35bf597a2075500cef3d67d2bb956d70e08d4d2
GyuSeong <moon01087209503@gmail.com>
2025-12-25 12:26:16 +0900

    마을, 저택 창고, 엔딩

---
+++ Assets/_Kamikakushi/Scripts/Contents/Object/InteractAction/EndingTriggerAction.cs
@@ -0,0 +1,21 @@
+﻿using _Kamikakushi.Contents.Player;
+using _Kamikakushi.Utills.Interfaces;
+using _Kamikakushi.Contents.Manager;
+using Project.Inventory;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+namespace _Kamikakushi.Contents.InteractAction
+{
+    /// <summary>
+    /// 내부의 IInteractAction을 딱 한번 실행하는 액션
+    /// </summary>
+    public class EndingTriggerAction : MonoBehaviour, IInteractAction
+    {
+        public void Execute(PlayerManager player, IInteractable source)
+        {
+            GameManagers.instance.trueEnding = true;
+        }
+    }
+}
---
+++ Assets/_Kamikakushi/Scripts/Contents/Object/Item/RequireEndingOption.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using _Kamikakushi.Contents.Manager;
+using _Kamikakushi.Contents.Player;
+using _Kamikakushi.Utills;
+using _Kamikakushi.Utills.Interfaces;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace _Kamikakushi.Contents.Item
+{
+    public class RequireEndingOption : MonoBehaviour, IInteractionCondition
+    {
+        public bool CanInteract(PlayerManager target)
+        {
+            return GameManagers.instance.trueEnding;
+        }
+    }
+
+}
